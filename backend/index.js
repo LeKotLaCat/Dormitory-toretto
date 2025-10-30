460fe67bf24def05c36f9a996cbf3afc9ec02854
AruchaK <aruchakhem@gmail.com>
2025-10-31 02:01:42 +0700

    test: allow cors all

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const corsOptions = {
   credentials: true, 
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
