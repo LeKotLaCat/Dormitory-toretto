862fbb00134ea5bab3f50fa5a0a31c5902805505
Petchpraethong Inuthai <tonkhaownj@gmail.com>
2025-11-04 03:07:19 +0700

    pls make this work

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,4 +1,4 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const API_URL = process.env.API_URL || 'http://backend-service:3000/api';
+export const API_URL = process.env.API_URL || 'http://10.43.101.170:3000/api';
